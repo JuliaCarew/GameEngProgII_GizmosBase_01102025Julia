a88b2ddc5353f31f9644398174a52f8fffca5b67
JuliaCarew <crewjulia3@gmail.com>
2025-10-01 14:58:37 -0300

    added bool toggles to enemy gizmo

--- Assets/scripts/enemyGizmo.cs
+++ Assets/scripts/enemyGizmo.cs
@@ -10,29 +10,40 @@ public class enemyGizmo : MonoBehaviour
 
     public Transform player;
 
+    [Header("Toggle Gizmos")]
+    public bool drawName = true;
+    public bool drawIcon = true;
+    public bool drawRadius = true;
+    public bool drawDistToPlayer = true;
+
+
     private void OnDrawGizmosSelected()
     {
+        Gizmos.color = enemyGizmoColor;
+
         // draw name label
-        UnityEditor.Handles.Label(transform.position + Vector3.up * 1.5f, $"{gameObject.name}");
+        if (drawName) UnityEditor.Handles.Label(transform.position + Vector3.up * 1.5f, $"{gameObject.name}");
 
         // draw icon
-        Gizmos.color = enemyGizmoColor;
-        Gizmos.DrawIcon(transform.position + Vector3.up * 2f, alertIcon, true);
+        if (drawIcon) Gizmos.DrawIcon(transform.position + Vector3.up * 2f, alertIcon, true);
 
-        Gizmos.DrawWireSphere(transform.position, radius);
+        if (drawRadius)  Gizmos.DrawWireSphere(transform.position, radius);
 
-        // midpoint to player
-        Vector3 midpoint = (transform.position + player.position) / 2f;
+        if (drawDistToPlayer)
+        {
+            // midpoint to player
+            Vector3 midpoint = (transform.position + player.position) / 2f;
 
-        // display distance
-        float distance = Vector3.Distance(transform.position, player.position);
+            // display distance
+            float distance = Vector3.Distance(transform.position, player.position);
 
-        // draw line to player
-        Gizmos.DrawLine(transform.position, player.position);
+            // draw line to player
+            Gizmos.DrawLine(transform.position, player.position);
 
-        // display red text of distance to player
-        GUIStyle style = new GUIStyle();
-        style.normal.textColor = Color.red;
-        UnityEditor.Handles.Label(midpoint, $"Distance: {distance:F2}", style);
+            // display red text of distance to player
+            GUIStyle style = new GUIStyle();
+            style.normal.textColor = Color.red;
+            UnityEditor.Handles.Label(midpoint, $"Distance: {distance:F2}", style);
+        }
     }
 }
