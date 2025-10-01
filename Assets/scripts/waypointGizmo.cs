72a501b3249e777461a4b7fb1dcd3decce96d9b1
JuliaCarew <crewjulia3@gmail.com>
2025-10-01 14:39:17 -0300

    add colored lines connecting waypoints

--- Assets/scripts/waypointGizmo.cs
+++ Assets/scripts/waypointGizmo.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class waypointGizmo : MonoBehaviour
@@ -6,11 +7,22 @@ public class waypointGizmo : MonoBehaviour
     public string icon = "idle_icon.png";
     public Color spawnColor;
 
+    // new
+    public Transform[] connectedWaypoints;
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawIcon(transform.position, icon);
 
         Gizmos.color = spawnColor;
         Gizmos.DrawSphere(transform.position, range);
+
+        foreach(Transform waypoint in connectedWaypoints)
+        {
+            var thickness = 3;
+
+            Handles.DrawBezier(transform.position, waypoint.position, transform.position, waypoint.position, Color.magenta, null, thickness);
+        }
     }
 }
+// draw from one waypoint to the next
